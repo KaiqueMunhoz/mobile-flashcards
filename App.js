622407b63b5008931e249e835a8e5d5d1e0ae467
Kaique Munhoz <munhoz13km@gmail.com>
2019-04-21 00:41:43 -0300

    Fix: Removendo codigo desnecessario

--- App.js
+++ App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
 import {ContainerView} from './style.js';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default class App extends React.Component {
-
-
+  
   render() {
-    
       return (
         <ContainerView>
-          <Text>Hello World</Text>
+          <AppNavigator />
         </ContainerView>
-         
-        
       );
     } 
   }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
