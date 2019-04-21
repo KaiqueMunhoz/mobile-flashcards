3f057dc5772fac3a3468b103e5f307337780e597
Kaique Munhoz <munhoz13km@gmail.com>
2019-04-21 15:36:07 -0300

    Feat: Criada tela `newDeck`

---
+++ src/screens/NewDeck/style.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native'
+
+export default StyleSheet.create({
+
+    title: {
+        marginTop: 15,
+        alignSelf: 'center'     
+    },
+
+    buttonSubmit : {
+        marginTop: 25,
+        marginLeft: 16,
+        marginRight: 16,
+    },
+
+    textSubmit : {
+        color: '#fff'
+    }
+})
