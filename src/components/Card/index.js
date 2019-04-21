8ec5b41840783f9ac4a8d14b0eb899f40487de78
Kaique Munhoz <munhoz13km@gmail.com>
2019-04-21 12:56:58 -0300

    Feat: Percorrendo dados da api

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {View} from 'react-native'
+import {View, Text} from 'react-native'
 
 const Card = () => {
 
     return(
         <View>
-            
+            <Text>Hello World</Text>
         </View>
     )
 }
--- src/screens/DecksList/index.js
+++ src/screens/DecksList/index.js
@@ -2,13 +2,31 @@ import React from 'react';
 import {
   View
 } from 'react-native';
+import { getData } from '../../utils/api'
+import Card from '../../components/Card'
 
 export default class HomeScreen extends React.Component {
 
+  state = {
+    decks : {}
+  }
+
+  componentDidMount() {
+    this.setState({
+      decks : getData()
+    })
+  }
+
   render() {
+    const {decks} = this.state
+
     return (
       <View>
-        
+        {
+          Object.keys(decks).map(deck => (
+            <Card key={deck}/>
+          ))
+        }
       </View>
     );
   }
