f6da0b5378276051b917a1aa91fb441d1912bc14
Kaique Munhoz <munhoz13km@gmail.com>
2019-04-24 00:19:07 -0300

    Feat: Configurando rotas

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
+
+//Screens
 import DeckScreen from '../screens/DeckScreen';
+import QuizScreen from '../screens/QuizScreen';
+import NewCardScreen from '../screens/NewCardScreen';
 
 export default createAppContainer(createSwitchNavigator({
   Main: MainTabNavigator,
-  DeckScreen
+  DeckScreen,
+  QuizScreen,
+  NewCardScreen,
 }));
