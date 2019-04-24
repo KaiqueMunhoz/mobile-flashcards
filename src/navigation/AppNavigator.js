c0c12c65d4422ba9d2149c3214db3a3e5e3fcca3
Kaique Munhoz <munhoz13km@gmail.com>
2019-04-23 23:12:15 -0300

    Feat: Criado rota para nova tela

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
+import DeckScreen from '../screens/DeckScreen';
 
 export default createAppContainer(createSwitchNavigator({
   Main: MainTabNavigator,
+  DeckScreen
 }));
