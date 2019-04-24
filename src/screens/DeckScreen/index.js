cfaa8299c6c7c638e44d23c0c0d70797f234ca78
Kaique Munhoz <munhoz13km@gmail.com>
2019-04-23 23:50:01 -0300

    Feat: Estilo inicial

--- src/screens/DeckScreen/index.js
+++ src/screens/DeckScreen/index.js
@@ -1,13 +1,37 @@
 import React from 'react'
-import {Container, Text} from 'native-base'
+import {Text, View} from 'react-native'
+import {Container, H1, H3, Content, Button} from 'native-base'
 
-const DeckScreen= () => {
+import style from './style'
 
-    return(
-        <Container>
-            <Text>Hello World</Text>
-        </Container>
-    )
+class DeckScreen extends React.Component {
+
+    componentDidMount() {
+        const {title, questions} = this.props
+    }
+
+    render() {
+        const {container, title, subtitle, addCard, titleAndSubtitle, textQuiz, startQuiz} = style
+
+        return(
+            <Container style={container}>
+                <Content>
+                    <View style={titleAndSubtitle}>
+                        <H1 style={title}>Javascript</H1>
+                        <H3 style={subtitle}>2 cards</H3>
+                    </View>
+
+                    <Button style={addCard} bordered info>
+                        <Text>Adicionar carta</Text>
+                    </Button>
+                    <Button style={startQuiz} info>
+                        <Text style={textQuiz} >Começar quiz</Text>
+                    </Button>
+                </Content>
+            </Container>
+        )
+    }
+    
 }
 
 export default DeckScreen
---
+++ src/screens/DeckScreen/style.js
@@ -0,0 +1,32 @@
+import {StyleSheet} from 'react-native'
+
+export default StyleSheet.create({
+
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    title: {
+        marginTop: 50,
+    },
+    subtitle: {
+        marginTop: 10,
+    },
+    addCard: {
+        marginTop: 50,
+        width: 200,
+        justifyContent: 'center',
+    },
+    startQuiz: {
+        marginTop: 10,
+        width: 200,
+        justifyContent: 'center',
+    },
+    titleAndSubtitle: {
+        alignItems: 'center'
+    },
+    textQuiz: {
+        color: '#fff'
+    }
+
+})
