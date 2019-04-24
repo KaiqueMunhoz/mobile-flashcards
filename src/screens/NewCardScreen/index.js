59ab39431fc4e67f7a7eb1e08e69e920ee56be65
Kaique Munhoz <munhoz13km@gmail.com>
2019-04-24 00:18:55 -0300

    Feat: Criando novas telas

---
+++ src/screens/NewCardScreen/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Text} from 'react-native'
+import {Container} from 'native-base'
+
+const NewCardScreen = () => {
+    <Container>
+        <Text>NewCard</Text>
+    </Container>
+}
+
+export default NewCardScreen
---
+++ src/screens/QuizScreen/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Text} from 'react-native'
+import {Container} from 'native-base'
+
+const QuizScreen = () => {
+    <Container>
+        <Text>Quiz</Text>
+    </Container>
+}
+
+export default QuizScreen
